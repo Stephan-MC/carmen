1ea05335d64516610dddb017c7228d684827201a
Minister <mundestephane13@gmail.com>
2024-05-22 08:21:22 +0100

    Added tailwindcss to the project

---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from "tailwindcss";
+
+export default {
+    content: ["resources/**/*.blade.php"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+} satisfies Config;
