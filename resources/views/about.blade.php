0c1a57d30bcb697c2f6bd01dde2446e0d133e4d5
Minister <mundestephane13@gmail.com>
2024-05-22 09:28:44 +0100

    Added the about page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'About Us')
+
+@section('content')
+    <div class="flex gap-6">
+        <span>This is the about page</span>
+        <span>Developed by: [Your Name]</span>
+    </div>
+@ensection
