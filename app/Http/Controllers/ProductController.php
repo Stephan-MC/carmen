151ed0c0147afefb90d1c6e636ad841e0584955c
Minister <mundestephane13@gmail.com>
2024-05-22 09:36:39 +0100

    product controller for the product related pages

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -13,7 +13,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        return view('products.index', ['products' => Product::all()]);
     }
 
     /**
@@ -21,7 +21,7 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        return view('products.create');
     }
 
     /**
@@ -45,7 +45,7 @@ class ProductController extends Controller
      */
     public function edit(Product $product)
     {
-        //
+        return view('product.edit');
     }
 
     /**
