733a402be976dd1528aac6f74942795e65370e25
Minister <mundestephane13@gmail.com>
2024-05-22 16:12:52 +0100

    Added logic for product edit page in controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -45,7 +45,7 @@ class ProductController extends Controller
      */
     public function edit(Product $product)
     {
-        return view('product.edit');
+        return view('admin.products.edit');
     }
 
     /**
