01adf6215222701b6df72dd342cca9124f345e3f
Minister <mundestephane13@gmail.com>
2024-05-22 08:41:32 +0100

    Registered product routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resources(
+    [
+        'products' => \App\Http\Controllers\ProductController::class,
+        'product' => \App\Http\Controllers\ProductController::class
+    ],
+    [
+        'products' => ['only' => 'index'],
+        'product' => ['except' => 'index']
+    ]
+);
