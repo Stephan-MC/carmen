53d0a294794f752970353a277e0799b16765f180
Minister <mundestephane13@gmail.com>
2024-05-22 08:55:03 +0100

    Added footer to the layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,26 +6,35 @@
     <title>Laravel Shop</title>
     @vite('resources/css/app.css')
 </head>
-<body>
-    <nav class="p-6 flex items-center justify-between text-white bg-slate-800">
-        <span class="text-xl font-bold">Online Store</span>
-        <div class="flex items-center [&>*]:space-x-4 [&>*]:px-4 divide-x divide-gray-200/50 font-semibold">
-            <div>
-                <a href="" class="">Home</a>
-                <a href="" class="">Products</a>
-                <a href="" class="">Cart</a>
-                <a href="" class="">Store</a>
+<body class="">
+    <div class="flex flex-col min-h-screen">
+        <nav class="p-6 flex items-center justify-between text-white bg-slate-800">
+            <span class="text-xl font-bold">Online Store</span>
+            <div class="flex items-center [&>*]:space-x-4 [&>*]:px-4 divide-x divide-gray-200/50 font-semibold">
+                <div>
+                    <a href="" class="">Home</a>
+                    <a href="" class="">Products</a>
+                    <a href="" class="">Cart</a>
+                    <a href="" class="">Store</a>
+                </div>
+                <div>
+                    @auth
+                        <a href="" class="">Logut</a>
+                    @else
+                        <a href="" class="">Login</a>
+                        <a href="" class="">Register</a>
+                    @endauth
+                </div>
             </div>
-            <div>
-                @auth
-                    <a href="" class="">Logut</a>
-                @else
-                    <a href="" class="">Login</a>
-                    <a href="" class="">Register</a>
-                @endauth
-            </div>
-        </div>
-    </nav>
-    @yield('content')
+        </nav>
+        <main class="grow">
+            @yield('content')
+        </main>
+
+        <footer class="bg-slate-800 flex items-center justify-center p-6 space-x-4 text-white">
+            <span class="text-gray-200">copyright</span>
+            <span class="font-semibold">Daniel Correa - Paola Vallejo</span>
+        </footer>
+    </div>
 </body>
 </html>
