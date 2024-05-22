f57f410cb3fbd640395310d681efa924c2704ab8
Minister <mundestephane13@gmail.com>
2024-05-22 08:50:42 +0100

    Added the navbar to the layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,25 @@
     @vite('resources/css/app.css')
 </head>
 <body>
+    <nav class="p-6 flex items-center justify-between text-white bg-slate-800">
+        <span class="text-xl font-bold">Online Store</span>
+        <div class="flex items-center [&>*]:space-x-4 [&>*]:px-4 divide-x divide-gray-200/50 font-semibold">
+            <div>
+                <a href="" class="">Home</a>
+                <a href="" class="">Products</a>
+                <a href="" class="">Cart</a>
+                <a href="" class="">Store</a>
+            </div>
+            <div>
+                @auth
+                    <a href="" class="">Logut</a>
+                @else
+                    <a href="" class="">Login</a>
+                    <a href="" class="">Register</a>
+                @endauth
+            </div>
+        </div>
+    </nav>
     @yield('content')
 </body>
 </html>
