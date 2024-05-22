d9cc1b5e548ae63da4c3f1008bfde0a282f97ade
Minister <mundestephane13@gmail.com>
2024-05-22 15:53:32 +0100

    updated the ui of the index page

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,11 +3,14 @@
 @section('title', 'List of products')
 
 @section('content')
-    <div class="grid xs: sm:grid-cols-3 md:grid-cols-4 gap-3">
+    <div class="grid xs: sm:grid-cols-3 md:grid-cols-4 gap-6">
         @foreach($products as $product)
-            <div class="flex flex-col items-center">
-                <img src="" class="aspect-square" />
-            </div>
+            <a href="/product/{{ $product->id }}">
+                <div class="flex flex-col items-center border p-2">
+                    <img src="" class="aspect-square w-full" />
+                    <span class="bg-green-400 text-white p-2 rounded capitalize">{{ $product->name }}</span>
+                </div>
+            </a>
         @endforeach
     </div>
 @endsection
