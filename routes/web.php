46d70b49f0a91aa9ac759f58b418a38c8d12b7a6
Minister <mundestephane13@gmail.com>
2024-05-23 14:11:22 +0100

    added a name for admin routes

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::resource('product', \App\Http\Controllers\ProductController::class)->only
 Route::view('cart', 'cart');
 Route::view('about', 'about');
 
-Route::prefix('admin')->group(function () {
+Route::prefix('admin')->name('admin.')->group(function () {
     Route::resource('products', \App\Http\Controllers\ProductController::class)->only('index');
     Route::resource('product', \App\Http\Controllers\ProductController::class)->except('index', 'show');
 });
