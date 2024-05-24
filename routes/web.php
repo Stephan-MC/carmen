650706c1dca008f394d8b1874ef081f0f1dae265
Minister <mundestephane13@gmail.com>
2024-05-24 11:11:27 +0100

    Added route to admin home page

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ Route::view('cart', 'cart');
 Route::view('about', 'about');
 
 Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('', function () { return 'Minister'; })->name('home');
     Route::resource('products', \App\Http\Controllers\ProductController::class)->only('index');
     Route::resource('product', \App\Http\Controllers\ProductController::class)->except('index', 'show');
 });
